22ce1550586bc4e0f22b1ffbac3e3c959700eb44
Eduardo Gonçalves <edu.goncalves99@gmail.com>
2019-04-19 18:31:09 -0300

    adicionei a bola

--- ep1.py
+++ ep1.py
@@ -4,11 +4,17 @@ cenarios = {
         'catraca_opcoes': {"Paquerar segurança": "Você tentará cortejar a/o segurança usando seu charme para te deixar passar sem a carteirinha", 
                           "Pular catraca": "Você tentará saltar por cima da catraca sem ser pego",
                           "Pedir": "Você pedirá accesso para a pessoa na secretaria"},
-        'catraca_consequencias': {'Paquerar segurança': 'Após dar em cima da/do segurança ela/ele não te deixou entrar e de sobra te avisou que você é feio/feia demais.',
+        'catraca_consequencias': {'Paquerar segurança': 'Após dar em cima da/do segurança ela/ele não te deixou entrar e de sobra te avisou que você é feio/feia demais. Você ficou com tanta vergonha que decidiu largar o Insper',
                                 'Pular catraca': 'Na sua tentativa de pular o seu pé ficou preso na catraca e você deu de cara no chão. Você teve que ir para o hospital e não pode falar com o professor.'},
         'briga_opcoes': {'Brigar': 'Você estufa o peito e sai pra briga.',
                          'Fugir': 'Você corre o mais rápido possivel para tentar escapar do jogador.'},
-        'briga_consequencias': {'Fugir': 'Obviamente, o jogador de rugbi te alcançou. Ele te fez coçegas até você morrer.'}
+        'briga_consequencias': {'Fugir': 'Obviamente, o jogador de rugbi te alcançou. Ele te fez coçegas até você morrer.'},
+        'bola_opcoes': {'Arriscar': 'Você arrisca e vai tentar pegar a bola para te ajudar em combates.',
+                        'Relevar': 'Você vai embora sem pegar a bola de futebol americano.'},
+        'bola_consequencias': {'Arriscar1': 'Você consegui pegar a bola sem ninguem te encher o saco! Vai ser uma potente arma nas suas batalhas.',
+                               'Arriscar2': 'A professora viu você atrapalhando a aula e te mandou para o diretor que te jubilou do Insper.',
+                               'Relevar': 'Você não pegou a bola, mas nada de ruim aconteceu contigo pelo menos!'},
+        
         
         }
 
@@ -36,9 +42,13 @@ def main():
     dano_seu= random.randint(10, 40)
     vida_rugby_pequeno = 20
     dano_rugby_pequeno = random.randint(1, 10)
+    azar= random.randint(0,5)
+    bola= False
+    dano_bola= random.randint(20,60)
     
     
-    
+########################## CENA 1 #############################################
+
     if not game_over:
         print("Dilema da catraca")
         print("-----------------")
@@ -68,14 +78,14 @@ def main():
             print()
             print('Como a sua escolha não estava nas opções ou você não sabe escrever direito você morreu de um ataque cardíaco repentino.')
             game_over=True
-    else:
-        print()
-        print('Você morreu :(')
-        game_over = True
+
     
     print()
     print()
     
+    
+########################## CENA 2 #############################################
+
     if not game_over:
         print('O primeiro ataque')
         print('-----------------')
@@ -117,12 +127,47 @@ def main():
                         print(cenarios['briga_consequencias']['Fugir'])
                         game_over=True
                         vida_rugby_pequeno= 0
+        else:
+            print()
+            print('Como a sua escolha não estava nas opções ou você não sabe escrever direito você morreu de um ataque cardíaco repentino.')
+            game_over=True
+
     
-
-
-    else:
-        print('Você perdeu :(')
-        game_over = True
+    print()
+    print()
         
+########################## CENA 3 #############################################
+        
+    if not game_over:
+        print('Sala de DesSoft')
+        print('---------------')
+        print()
+        print('Como você está a procura do professor de DesSoft, você decide procurar por ele na sala onde ele te da aula. No entanto, quando você olha na sala esta tendo a aula de outra materia com outro professor. Decepcionado, voce vai ir a outra aula procurar ele mas você vê no fundo dessa aula em andamento uma bola de futebol americano, uma potencial arma muito útil contra seus inimigos do rugby.')
+        print()
+        print('As suas opções são: ')
+        print()
+        opcoes= cenarios['bola_opcoes'].keys()
+        for key, value in cenarios['bola_opcoes'].items():
+            print("{}: {}".format(key, value))
+            print()
+        escolha = input("Qual é a sua escolha? ")
+        if escolha in opcoes:
+            if escolha == 'Arriscar':
+                if azar<4:
+                    print()
+                    print(cenarios['bola_consequencias']['Arriscar1'])
+                    bola= True
+                else:
+                    print()
+                    print(cenarios['bola_consequencias']['Arriscar2'])
+                    game_over= True
+            elif escolha == 'Relevar':
+                print()
+                print(cenarios['bola_consequencias']['Relevar'])
+        else:
+            print()
+            print('Como a sua escolha não estava nas opções ou você não sabe escrever direito você morreu de um ataque cardíaco repentino.')
+            game_over=True
+
         
 print(main())
