162a4dc14ed53fbbd6254d757961734a06fee228
Eduardo Gonçalves <edu.goncalves99@gmail.com>
2019-04-19 16:34:04 -0300

    mudei a parada toda

--- ep1.py
+++ ep1.py
@@ -1,84 +1,62 @@
-def carregar_cenarios():
-    cenarios = {
-        "inicio": {
-            "titulo": "Saguao do perigo",
-            "descricao": "Voce esta no saguao de entrada do insper",
-            "opcoes": {
-                "andar professor": "Tomar o elevador para o andar do professor",
-                "biblioteca": "Ir para a biblioteca"
-            }
-        },
-        "andar professor": {
-            "titulo": "Andar do desespero",
-            "descricao": "Voce chegou ao andar da sala do seu professor",
-            "opcoes": {
-                "inicio": "Tomar o elevador para o saguao de entrada",
-                "professor": "Falar com o professor"
-            }
-        },
-        "professor": {
-            "titulo": "O monstro do Python",
-            "descricao": "Voce foi pedir para o professor adiar o EP. "
-                         "O professor revelou que é um monstro disfarçado "
-                         "e devorou sua alma.",
-            "opcoes": {}
-        },
-        "biblioteca": {
-            "titulo": "Caverna da tranquilidade",
-            "descricao": "Voce esta na biblioteca",
-            "opcoes": {
-                "inicio": "Voltar para o saguao de entrada"
-            }
+
+cenarios = {
+        'catraca_opcoes': {"Paquerar segurança": "Você tentará cortejar a/o segurança usando seu charme para te deixar passar sem a carteirinha", 
+                          "Pular catraca": "Você tentará saltar por cima da catraca sem ser pego",
+                          "Pedir": "Você pedirá accesso para a pessoa na secretaria"},
+        'catraca_consequencias': {'Paquerar segurança': 'Após dar em cima da/do segurança ela/ele não te deixou entrar e de sobra te avisou que você é feio/feia demais.',
+                                'Pular catraca': 'Na sua tentativa de pular o seu pé ficou preso na catraca e você deu de cara no chão. Você teve que ir para o hospital e não pode falar com o professor.'},
+        
+        
+        
         }
-    }
-    nome_cenario_atual = "inicio"
-    return cenarios, nome_cenario_atual
+
+opcoes=[]
+escolha= "nenhuma"
+
+
+
+print("Na hora do sufoco!")
+print("------------------")
+print()
+print("Parecia uma boa idéia: vou só jogar um pouquinho/assistir Netflix/"
+        "embaçar em geral. Amanhã eu começo o EP. Mas isso não deu certo...")
+print()
+print("É o dia de entregar o EP e você está muuuuito atrasado! Você está "
+        "na entrada do Insper, e quer procurar o professor para pedir um "
+        "adiamento do EP (boa sorte...)")
+print()
+
 
 
 def main():
-    print("Na hora do sufoco!")
-    print("------------------")
-    print()
-    print("Parecia uma boa idéia: vou só jogar um pouquinho/assistir Netflix/"
-        "embaçar em geral. Amanhã eu começo o EP. Mas isso não deu certo...")
-    print()
-    print("É o dia de entregar o EP e você está muuuuito atrasado! Você está "
-        "na entrada do Insper, e quer procurar o professor para pedir um "
-        "adiamento do EP (boa sorte...)")
-    print()
-
-    cenarios, nome_cenario_atual = carregar_cenarios()
-
     game_over = False
-    while not game_over:
-        cenario_atual = cenarios[nome_cenario_atual]
-
+    if not game_over:
         print("Dilema da catraca")
         print("-----------------")
-        print("Você chegou no Insper e está do lado da catraca quando você descobre que esqueceu a carteirinha")
+        print("Você chegou no Insper e está do lado da catraca quando você descobre que esqueceu a carteirinha.")
         print()
-        
-        opcoes = cenario_atual['opcoes']
-        if len(opcoes) == 0:
-            print("Acabaram-se suas opções! Mwo mwo mwooooo...")
-            game_over = True
+        print('As suas opções são: ')
+        print()
+        opcoes= cenarios['catraca_opcoes'].keys()
+        for key, value in cenarios['catraca_opcoes'].items():
+            print("{}: {}".format(key, value))
+        print()
+        escolha = input("Qual é a sua escolha? ")
+        if escolha in opcoes:
+            if escolha == 'Pedir':
+                print('A sua escolha foi a certa!')
+            elif escolha == 'Paquerar segurança':
+                print(cenarios['catraca_consequencias']['Paquerar segurança'])
+                game_over=True
+            elif escolha == 'Pular catraca':
+                print(cenarios['catraca_consequencias']['Pular catraca'])
+                game_over=True
         else:
-            print("escolha sua opcao")
-            print()
-            print(opcoes.items())
-            escolha = input("escolha seu cenario")
-
-            if escolha in opcoes:
-                nome_cenario_atual = escolha
-            else:
-                print("Sua indecisão foi sua ruína!")
-                game_over = True
-
-    print("Você morreu!")
-
-
-# Programa principal.
-if __name__ == "__main__":
-    main()
-    
-####
+            print('Como a sua escolha não estava nas opções ou você não sabe escrever direito você morreu de um ataque cardíaco repentino.')
+            game_over=True
+    else:
+        print()
+        game_over = True
+        
+        
+print(main())
